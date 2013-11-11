a3f60a4d4a47b617a3c53a7308018c49cc65a058
Vadim <trey5511@gmail.com>
2013-11-11 22:45:21 +0200

    nn

--- test2.js
+++ test2.js
@@ -1,12 +1,8 @@
 var arr = [31, 41, 59, 5, 26, 41, 58, 21];
-var number = 5;
-var numIndex
 
-for(var i = 0; i < arr.length; i++){
-	if(arr[i] == number){
-		numIndex = i;
-	}
-}
+var number = 5;
+var numIndex = arr.indexOf(number);
+
 
 console.log(numIndex);
 
