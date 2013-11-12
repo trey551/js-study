233fc1d10e63907fb21ddcf7ef990d3bdb8e4081
Vadim <trey5511@gmail.com>
2013-11-12 20:46:47 +0200

    kill excess code

--- lesson1/task2.js
+++ lesson1/task2.js
@@ -11,20 +11,17 @@ var arr = [31, 41, 59, 5, 26, 41, 58, 21];
 // console.log(numIndex);
 
 function findNum(arr, number){
-	var result;
+	var result = null;
 	for(var i = 0; i < arr.length; i++){
 		if(arr[i] == number){
 			result = i;
 			break;
 		}
 	}
-	if(result == undefined){
-		result = null;
-	}
 	return result;
 }
 
-var numIndex = findNum(arr, 5);
+var numIndex = findNum(arr, 55);
 console.log(numIndex);
 
 //определить индекс number в массиве
