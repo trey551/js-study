43d88dbffb3e9f36b635d0c4a05c032dd7c7a223
Vadim <trey5511@gmail.com>
2013-11-13 22:46:53 +0200

    Update task5.js

--- lesson1/task5.js
+++ lesson1/task5.js
@@ -2,13 +2,18 @@ var arr1 = [10, 40, 33];
 var arr2 = [5, 6];
 
 function maxSumm(arr1, arr2){
+	
+	
+	
+	
 	var summ1 = 0;
 	for(var i = 0; i < arr1.length; i++){
 		summ1 = summ1 + arr1[i];
 	}
 	return summ1;
+	summ
+	//////
 }
-
 console.log(maxSumm(arr1, arr2));
 
 
