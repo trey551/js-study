7456c6f7f2e4e1451d991508a4765faeab28cdab
Vadim <trey5511@gmail.com>
2013-11-18 23:01:24 +0200

    buble_sort complete

--- lesson1/buble_sort.js
+++ lesson1/buble_sort.js
@@ -1,6 +1,16 @@
 var arr = [343, 3, 45, 65, 6, 57, 8, 321];
 
-function bubleSort(){
-	
+function bubleSort(arr){
+	for(var i = 0; i < arr.length; i++){
+		for (var j = i; j < arr.length; j++) {
+			if(arr[i] > arr[j]){
+				var exch = arr[j];
+				arr[j] = arr[i];
+				arr[i] = exch;
+			}
+		};
+	}
+	return arr;
 }
 
+console.log(bubleSort(arr));
