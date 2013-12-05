f502a0dce98a9977ceb312d272b87b379ae47f3d
Vadim <trey5511@gmail.com>
2013-12-06 00:54:21 +0200

    random block update

--- RandomImage/js/main.js
+++ RandomImage/js/main.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', main, false);
 function main(){
-    var holder = document.querySelector('.holder');
-    getRandomBlock(holder, {
-        selector: "div.holder img"
+    getRandomBlock({
+        holder: "div.holder",
+        selector: "img"
     });
 }
 
-function getRandomBlock(holder, opts){
-    var blocks = document.querySelectorAll(opts.selector);
+function getRandomBlock(opts){
+    var sel = '', space = ' ';
+    sel = sel.concat(opts.holder, space, opts.selector);
+    var blocks = document.querySelectorAll(sel);
+    for(var i = 0; i < blocks.length; i++){
+        blocks[i].style.display = 'none';
+    }
     function getRandomNumber(num){
         var randomNumber = Math.floor(Math.random() * num);
         return randomNumber;
