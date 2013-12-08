4f1db0a73bd3e3df31c4f88b5897d761d85e7857
Vadim <trey5511@gmail.com>
2013-12-08 20:06:32 +0200

    up

--- openclose/js/main.js
+++ openclose/js/main.js
@@ -1,56 +1,48 @@
-
 document.addEventListener("DOMContentLoaded", main, false);
 
 function main() {
-    var holder = document.querySelectorAll('.holder');
-
-    var openClose = new OpenClose(holder, {
-        openSlide: "a.opener",
-        slides: "div.slide",
-        setPosition: false
+    var holders = document.querySelectorAll('.holder');
+    Array.prototype.forEach.call(holders, function(holder, i) {
+        var openClose = new OpenClose(holder, {
+            openSlide: "a.opener",
+            slides: "div.slide",
+            setPosition: false
+        });
     });
 
 }
 
 function OpenClose(holder, opts) {
     this.holder = holder;
-    this.openSlide = document.querySelectorAll(opts.openSlide);
-    this.slides = document.querySelectorAll(opts.slides);
+    this.openSlide = holder.querySelector(opts.openSlide);
+    this.slides = holder.querySelector(opts.slides);
     this.setPosition = opts.setPosition;
 
-
-    this.setOpenClose(this.setPosition);
+    this.toggleSlide();
     this.addEvents();
 }
 
-OpenClose.prototype.setOpenClose = function(flag) {
-    var self = this;
-
-    Array.prototype.forEach.call(this.slides, function(slide, i) {
-        if (flag) {
-            slide.style.display = "block";
-        } else {
-            slide.style.display = "none";
-        }
-    });
-}
 OpenClose.prototype.addEvents = function() {
     var self = this;
-    Array.prototype.forEach.call(this.openSlide, function(openSlide, i) {
-        openSlide.addEventListener("click", function() {
-            if (!self.setPosition) {
-                self.showSlide(i);
-            }else{
-                self.hideSlide(i);
-            }
-        }, false);
-    });
+    this.openSlide.addEventListener("click", function() {
+        self.toggleSlide();
+    }, false);
 }
-OpenClose.prototype.showSlide = function(index) {
-    this.slides[index].style.display = "block";
-    this.setPosition = true;
+
+OpenClose.prototype.toggleSlide = function() {
+    if (this.setPosition) {
+        this.showSlide();
+    } else {
+        this.hideSlide();
+    }
 }
-OpenClose.prototype.hideSlide = function(index) {
-    this.slides[index].style.display = "none";
+
+OpenClose.prototype.showSlide = function() {
+    this.slides.style.display = "block";
     this.setPosition = false;
+}
+
+OpenClose.prototype.hideSlide = function() {
+    this.slides.style.display = "none";
+    this.setPosition = true;
 }
