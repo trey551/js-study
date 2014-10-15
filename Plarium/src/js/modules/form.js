e95fef6cddd4804c783189ccc6fbb4f56a11495f
Trey <trey5511@gmail.com>
2014-10-15 23:38:28 +0300

    complete Plarium task

--- Plarium/src/js/modules/form.js
+++ Plarium/src/js/modules/form.js
@@ -1,62 +1,14 @@
 define([
-    'jQuery'
+    'jQuery',
+    'domReady'
 ], function ($) {
-    initValidateForm();
-    facebookRequest();
-    sendForm();
+    initPlaceholder();
+    initFacebookButton();
+    initValidation();
+    initForm();
 
-    function sendForm() {
-        $(".btn_play").on("click", function () {
-            var errors = 0;
-            var emptyFields = [];
-
-            $("#form .row input").each(function () {
-                if ($(this).parent().hasClass("error") || $(this).parent().hasClass("error2")) {
-                    errors++;
-                }
-                if ($(this).val() == "") {
-                    errors++;
-                    emptyFields.push($(this).attr("name"));
-                }
-            });
-            if (!$("#ch_agree").prop("checked")) {
-                errors++;
-            }
-            if (errors == 0) {
-                var msg = $('#form').serialize();
-                if ($("#valkirie").hasClass("selected")) {
-                    var url = "http://posttestserver.com/post.php?dump&html&dir=JSLandingPageVlk";
-                } else if ($("#soldier").hasClass("selected")) {
-                    var url = "http://posttestserver.com/post.php?dump&html&dir=JSLandingPageTrp";
-                }
-                $.ajax({
-                    type: 'POST',
-                    url: url,
-                    data: msg,
-                    success: function (data) {
-                        console.log(data);
-                    },
-                    error: function () {
-                        console.log("Ошибка!");
-                    }
-                });
-                return false;
-            } else {
-                requiredFieldsError(emptyFields);
-            }
-            function requiredFieldsError(req) {
-                for (var i = 0; i <= req.length; i++) {
-                    $(".field_error." + req[i] + "_req").slideDown();
-                    $(".row." + req[i] + "").addClass('error');
-                }
-            }
-
-            return false;
-        });
-    }
-
-    function facebookRequest() {
-        $(".facebook").on("click", function () {
+    function initFacebookButton() {
+        $(".facebook").on("click", function (e) {
             $.ajax({
                 url: 'http://www.json-generator.com/api/json/get/bZtGcVPsrS?indent=2',
                 success: function (data) {
@@ -68,77 +20,180 @@ define([
                     console.log("Error!");
                 }
             });
-            return false;
+
+            e.preventDefault();
         });
     }
 
-    function initValidateForm() {
-        $("#form input").on("keyup", function () {
-            var name = $(this).attr("name");
-            var field = $("input[name=" + name + "]").val();
-            if (name == "email") {
-                var pattern = /^[a-z_][\w-]+@(\w{2,16}\.){1,3}[a-z]{2,6}$/i;
-            } else if (name == "username") {
-                var pattern = /^[A-Za-z0-9]{3,16}$/i;
-            } else if (name == "password" || name == "password2") {
-                if (name == "password2") matchFields();
-                var pattern = /^[A-Za-z0-9]{6,40}$/i;
+    function initValidation() {
+        var form = $('#form');
+        var submitBtn = $('.btn_play', form);
+        var fields = $('input[data-valid]', form);
+        var fieldRows = fields.parent();
+
+        fields.each(function (i) {
+            $(this).on('keyup validate', function () {
+                onKeyUpHandler(i);
+            });
+        });
+
+        function onKeyUpHandler(i) {
+            var row = fieldRows.eq(i);
+            var field = fields.eq(i);
+            var value = field.val();
+            var validAttr = field.attr('data-valid');
+            var errorMessageAttr = field.attr('data-error-message');
+            var errorMessage = $('.field_error.' + errorMessageAttr, form);
+            var isValid = true;
+
+            switch (validAttr) {
+                case 'email':
+                    validateEmailField();
+                    break;
+                case 'username':
+                    validateUserName();
+                    break;
+                case 'password':
+                    validatePassword();
+
+                    if (field.attr('data-field-match')) {
+                        validateMatch();
+                    }
+
+                    break;
             }
-            if (field != "") {
-                $(".field_error." + name + "_req").slideUp();
-            }
-            if (field.search(pattern) == -1) {
-                $(".field_error." + name + "").slideDown();
-                $(".row." + name + "").addClass('error');
+
+            if (isValid) {
+                hideError();
             } else {
-                $(".field_error." + name + "").slideUp();
-                $(".row." + name + "").removeClass('error');
+                showError();
             }
-            function matchFields() {
-                var pass1 = $("input[name=password]").val();
-                var pass2 = $("input[name=password2]").val();
-                if (pass1 != pass2) {
-                    $(".field_error.nomach").slideDown();
-                    $(".row.password2").addClass('error2');
-                } else {
-                    $(".field_error.nomach").slideUp();
-                    $(".row.password2").removeClass('error2');
+
+            function validateEmailField() {
+                var pattern = /^[a-z_][\w-]+@(\w{2,16}\.){1,3}[a-z]{2,6}$/i;
+
+                if (!pattern.test(value)) {
+                    isValid = false;
                 }
             }
+
+            function validateUserName() {
+                var pattern = /^[A-Za-z0-9]{3,16}$/i;
+
+                if (!pattern.test(value)) {
+                    isValid = false;
+                }
+            }
+
+            function validatePassword() {
+                var pattern = /^[A-Za-z0-9]{6,40}$/i;
+
+                if (!pattern.test(value)) {
+                    isValid = false;
+                }
+            }
+
+            function validateMatch() {
+                var matchField = form.find('input[name=' + field.attr('data-field-match') + ']');
+                var errorMessage = row.nextAll('.nomach');
+
+                if (value != matchField.val()) {
+                    errorMessage.slideDown();
+                    row.add(field).addClass('error');
+                } else {
+                    errorMessage.slideUp();
+                    row.add(field).removeClass('error');
+                }
+            }
+
+            function showError(validAttr) {
+                errorMessage.slideDown();
+                row.add(field).addClass('error');
+            }
+
+            function hideError(validAttr) {
+                errorMessage.slideUp();
+                row.add(field).removeClass('error');
+            }
+        }
+    }
+
+    function initForm () {
+        var form = $('#form');
+
+        form.on('submit', function (e) {
+            var fields =  $('input[data-valid]');
+
+            //force all field to being validated
+            fields.trigger('validate');
+
+            var errorFields = fields.filter('.error');
+
+            if (!errorFields.length) {
+                var msg = $('#form').serialize(), url;
+
+                if ($("#valkirie").hasClass("selected")) {
+                    url = "http://posttestserver.com/post.php?dump&html&dir=JSLandingPageVlk";
+                } else if ($("#soldier").hasClass("selected")) {
+                    url = "http://posttestserver.com/post.php?dump&html&dir=JSLandingPageTrp";
+                }
+
+                $.ajax({
+                    type: 'POST',
+                    url: url,
+                    data: form.serialize(),
+                    success: function (data) {
+                        console.log(data);
+                    },
+                    error: function () {
+                        console.log('Error has occurred!');
+                    }
+                });
+            }
+
+            e.preventDefault();
         });
     }
 
-    $(function(){
-        function hasPlaceholderSupport(){
+    function initPlaceholder() {
+        function hasPlaceholderSupport() {
             var input = document.createElement('input');
             return ('placeholder' in input);
         }
 
         if (!hasPlaceholderSupport()) {
-        $('[placeholder]').focus(function(){
-            var input = $(this);
-            if (input.val() == input.attr('placeholder')) {
-                input.val('');
-                input.removeClass('placeholder');
-                if (input.attr('data-type')=='password'){
-                    marker = $('<span />').insertBefore(input);
-                    input.detach().attr('type', 'password').insertAfter(marker);
-                    setTimeout(function() { input.focus(); }, 10); 
-                    marker.remove();
+            $('[placeholder]').focus(function () {
+                var input = $(this), marker;
+                if (input.val() == input.attr('placeholder')) {
+                    input.val('');
+                    input.removeClass('placeholder');
+                    if (input.attr('data-type') == 'password') {
+                        marker = $('<span />').insertBefore(input);
+                        input.detach().attr('type', 'password').insertAfter(marker);
+                        setTimeout(function () {
+                            input.focus();
+                        }, 10);
+                        marker.remove();
+                    }
                 }
-            }
-        }).blur(function(){
-            var input = $(this);
-            if (input.val() == '' || input.val() == input.attr('placeholder')) {
-                input.addClass('placeholder');
-                input.val(input.attr('placeholder'));
-                if (input.attr('type')=='password'){
-                    marker = $('<span />').insertBefore(input);
-                    input.detach().attr('type', 'text').attr('data-type', 'password').insertAfter(marker);
-                    marker.remove();
+            }).blur(function () {
+                var input = $(this), marker;
+                if (input.val() == '' || input.val() == input.attr('placeholder')) {
+                    input.addClass('placeholder');
+                    input.val(input.attr('placeholder'));
+                    if (input.attr('type') == 'password') {
+                        marker = $('<span />').insertBefore(input);
+                        input.detach().attr('type', 'text').attr('data-type', 'password').insertAfter(marker);
+                        marker.remove();
+                    }
                 }
-            }
-        }).blur();
+            }).blur();
+        }
     }
-    });
 });
+
+
+
+
+
+
