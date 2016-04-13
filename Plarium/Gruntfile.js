0db70f31be8688f83fded98983667b1cbdaee25a
Vadim Itera <trey5511@gmail.com>
2016-04-13 17:54:32 +0300

    added tasks

--- Plarium/Gruntfile.js
+++ Plarium/Gruntfile.js
@@ -27,6 +27,7 @@ module.exports = function (grunt) {
                        "<%= build_dir %>/css/main.css": [
                            '<%= bower_dir %>/animate.css/animate.css',
                            '<%= src_dir %>/**/*.less',
+                           '<%= src_dir %>/css/*.css',
                            '!<%= src_dir %>/less/ie.less'
                        ],
                        "<%= build_dir %>/css/ie.css": [
@@ -34,6 +35,18 @@ module.exports = function (grunt) {
                        ]
                    }
                }
+        },
+
+        cssmin: {
+          target: {
+            files: [{
+              expand: true,
+              cwd: '<%= build_dir %>/css',
+              src: ['*.css', '!*.min.css'],
+              dest: '<%= build_dir %>/css',
+              ext: '.min.css'
+            }]
+          }
         },
 
         watch: {
@@ -75,18 +88,63 @@ module.exports = function (grunt) {
                     optimize: 'none'
                 }
             }
-        }
+        },
+        sprite:{
+          all: {
+            src: '<%= src_dir %>/images/sprite/*.png',
+            dest: '<%= src_dir %>/images/sprite.png',
+            destCss: '<%= src_dir %>/css/sprite.css'
+          }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        '<%= build_dir %>/css/*.css',
+                        '<%= build_dir %>/*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: '<%= build_dir %>'
+                }
+            }
+        },
+        imagemin: {                          // Task 
+              target: {
+                  files: [{
+                    expand: true,                  // Enable dynamic expansion 
+                    cwd: '<%= src_dir %>',                   // Src matches are relative to this path 
+                    src: ['images/*.{png,jpg,gif}'],   // Actual patterns to match 
+                    dest: '<%= build_dir %>'                  // Destination path prefix 
+                  }]
+              }
+          }
     };
 
     grunt.initConfig(config);
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
 
-    grunt.registerTask('build', ['clean', 'requirejs', 'copy', 'less']);
+    grunt.registerTask('build', [
+        'clean',
+        'requirejs',
+        'sprite',
+        'copy',
+        'less',
+        'cssmin',
+        // 'imagemin',
+        'browserSync'
+    ]);
 
     grunt.registerTask('start', [
         'build',
